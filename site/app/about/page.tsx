1fe0e977cfc361561effd771d37a85ba78e13259
aqua <84078890+phthallo@users.noreply.github.com>
2025-05-15 17:02:29 -0400

    a

--- site/app/about/page.tsx
+++ site/app/about/page.tsx
@@ -4,7 +4,9 @@ export default function About(){
     return (
     <>
         <Header skipWelcomeModal={true}/>
-        <div className = "flex flex-col w-screen pt-24 p-16 min-h-screen bg-hc-primary-dull bg-[url(/bg.svg)]">
+        <img className = "object-cover w-screen h-[50vh]" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/390b6fa1417d3af121ebf065b59e7ad25052c811_image.png"/>
+
+        <div className = "flex flex-col w-screen p-16 min-h-screen bg-hc-primary-dull bg-[url(/bg.svg)]">
             <div className="self-start">
           <a
             className="text-xl sm:text-2xl uppercase text-white font-bold mb-2"
@@ -32,7 +34,7 @@ export default function About(){
             <p>Can't make it to New York City? No stress - for every hour you code during the Athena Award, you'll be rewarded with one <span className = "!text-gold">artifact</span>.</p>
             <p>Artifacts can be redeemed in our shop for <span className = "!text-gold font-bold">laptops, hoodies, headphones</span> and more, to help you keep on hacking!</p>
           
-            <p>Additionally, all successful recipients of the Athena Award will be sent a physical certificate </p>
+            <p>Additionally, all successful recipients of the Athena Award will be sent a certificate - available in digital and physical form - to certify their technical skills. </p>
             <h2 className = "!text-3xl">How can I get started?</h2>
             <AuthStateButton/>
             <p>Sign up here to access the Athena Award website, where you'll be able to:</p>
@@ -44,9 +46,9 @@ export default function About(){
             </ul>
             <p>Additionally, you'll be sent an invite to join the Hack Club Slack - a community of high school programmers from all around the world. If you need help on your project and getting started, the Slack is the place to be!</p>
         
-            <h2 className = "!text-3xl">Okay, I'm sold. How can I start working on my project?</h2>
+            <h2 className = "!text-3xl">Okay, I'm sold. How can I start working on my projects?</h2>
             <p>First, figure out what you want to make. If you're a complete beginner, start with the Beginner Track - three project ideas that we guide you through completing.</p>
-            <p>The first one, <a target = "_blank" href = "https://tribute.hackclub.com">Tribute</a>, will teach you how to make a basic website themed after something you like. By the end of the Beginner Track, you'll be prepared to start making some awesome webpages!</p>
+            <p>The first one, <a target = "_blank" href = "https://tribute.hackclub.com">Tribute</a>, will teach you how to make a basic website themed after something you like. By the end of the Beginner Track, you'll be prepared to continue making some awesome, functional webpages!</p>
             <p>Alternatively, if you already know a little bit of programming - feel free to jump in with the Advanced Track.</p>
             <div className = "*:border-[#E89368] *:rounded-lg *:border-2 flex flex-col *:flex-1 md:flex-row gap-4 *:basis-1/3 *:grow justify-center *:bg-[#E89368]/30 *:p-4 items-center">
                 <div>
@@ -58,8 +60,14 @@ export default function About(){
                     <p>Build three of your own projects - for people who want to challenge themselves! Things for work and school don't count for the Athena Award!</p>
                 </div>
             </div>
-            <h2 className = "!text-3xl">How do I submit my project?</h2>
+            <h2 className = "!text-3xl">How do I submit my projects?</h2>
             <p>Your project needs to meet some criteria before you submit.</p>
+            <ul className = "list-disc list-inside">
+                <li>Code needs to be open source. Use a website like <a target = "_blank" href = "https://github.com">GitHub</a> to publish your code.</li>
+                <li>Your project needs to, well, exist. It doesn't count if you're the only one who can access it! For instance, websites need to have a public URL, and apps need to be downloadable. This is called <span className = "!text-gold">deploying</span> your project.</li>
+                <li>It needs to be something original, that's not for work or school. Also - come on, you can do better than using AI to write your entire project for you.</li>
+            </ul>
+            <p>Once that's done, a member of the team will review and approve your project. Your project might be rejected if it doesn't meet the criteria, in which case, you're free to resubmit once you fix any issues.</p>
           </div>
           </div>
         </div>
