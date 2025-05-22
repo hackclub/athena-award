a1831c7b1d4ed4a557097856ef6862ecc2ff1eb9
aqua <84078890+phthallo@users.noreply.github.com>
2025-05-22 00:52:14 -0400

    i love you too coolify

--- site/hash.sh
+++ site/hash.sh
@@ -5,6 +5,3 @@ npm install
 HASH=$(git log -n1 --format="%h")
 LONG_HASH=$(git log --pretty=format:'%H' -n 1)
 sed -i 's@<li>For example, you can see the current commit of this website here: </li>@<li>For example, you can see the current commit of this website here: <a href="https://github.com/hackclub/athena-award/commit/'$LONG_HASH'">'$HASH'</a></li>@g' app/onboarding/page.tsx
-
-npm run build
-npm run start
