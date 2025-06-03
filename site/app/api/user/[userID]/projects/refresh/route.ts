11422569ea124edc3c5298f4b61c0b2f2c03141e
aqua <84078890+phthallo@users.noreply.github.com>
2025-06-03 13:27:17 -0400

    add auth to refresh

--- site/app/api/user/[userID]/projects/refresh/route.ts
+++ site/app/api/user/[userID]/projects/refresh/route.ts
@@ -15,6 +15,12 @@ export async function POST(
   { params }: { params: Promise<{ userID: string }> },
 ) {
   const stageNumber = request.nextUrl.searchParams.get("stage");
+  const authorization = request.headers.get("Authorization")
+
+  if (!authorization || authorization.split(" ")[1] != process.env.REFRESH_API_KEY ){
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  }
+
   const slackId = (await params).userID;
   try {
     const recordID = await airtable("Registered Users")
