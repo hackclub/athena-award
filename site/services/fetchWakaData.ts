3a1168feba12b12186e01508cf4c130669027185
aqua <84078890+phthallo@users.noreply.github.com>
2025-06-28 18:01:42 -0400

    bypass hackatime ratelimits

--- site/services/fetchWakaData.ts
+++ site/services/fetchWakaData.ts
@@ -5,6 +5,10 @@ const end_date = process.env.NEXT_PUBLIC_END_DATE;
 export async function getWakaTimeData(slackId: string) {
   const response = await fetch(
     `https://hackatime.hackclub.com/api/v1/users/${slackId}/stats?features=projects&start_date=${start_date}&end_date=${end_date}`,
+      { 
+        headers: { "Rack-Attack-Bypass": process.env.HACKATIME_RATE_LIMIT_BYPASS!
+      }
+    }
   );
   console.log(response)
   return response;
